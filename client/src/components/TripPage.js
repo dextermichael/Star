a8cd6f7fe0015356577266efbcc815dbbe8bd610
dextermichaelh <dextermichael.h@gmail.com>
2018-11-19 01:37:55 -0500

    bug?

---
+++ client/src/components/TripPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TripPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default TripPage;
